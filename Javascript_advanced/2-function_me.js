4b729fb6d9fd57f9abad39d37fe0d86d62f6f92b
Tribeoftech <gratefulgrowing333@gmail.com>
2023-11-10 16:56:05 -0600

    fixing 2 semi/bracket tack one spot away from optimal

--- Javascript_advanced/2-function_me.js
+++ Javascript_advanced/2-function_me.js
@@ -1,12 +1,12 @@
-function createClassRoom(numberOfStudents);
+function createClassRoom(numbersOfStudents) {
     function studentSeat(seat) {
       return () => {
         return seat;
-     
+      };
     }
     let students = [];
     for (let i = 0; i < numbersOfStudents; i++) {
-      let students_seats = studentSeat (i + 1);
+      let students_seats = studentSeat(i + 1);
       students[i] = students_seats;
     }
     return students;
